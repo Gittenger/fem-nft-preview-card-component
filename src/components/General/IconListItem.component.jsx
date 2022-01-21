ca082fed66541b718ea1257bce03c2791b0f11de
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-21 02:39:55 -0500

    color + iconWidth props

--- src/components/General/IconListItem.component.jsx
+++ src/components/General/IconListItem.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 
-const IconListItem = ({ imgSrc, altText, textContent }) => {
+const IconListItem = ({
+  imgSrc,
+  altText,
+  textContent,
+  color,
+  iconWidth,
+  className,
+}) => {
   const groupStyles = `flex justify-start items-center`
-  const iconStyles = 'w-2.5'
+  const iconStyles = `${iconWidth === 'sm' ? 'w-2.5' : 'w-3.5'}`
   const textStyles = `text-md flex items-center justify-center ml-2`
 
   return (
-    <div className={groupStyles}>
+    <div className={`${groupStyles} ${className}`}>
       <img className={iconStyles} src={imgSrc} alt={altText} />
-      <p className={`text-cyan font-semibold ${textStyles}`}>{textContent}</p>
+      <p
+        className={`${
+          color === 'cyan' ? 'text-cyan' : ''
+        } font-semibold ${textStyles}`}
+      >
+        {textContent}
+      </p>
     </div>
   )
 }
