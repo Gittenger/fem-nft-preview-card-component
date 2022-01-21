47518e738c1737818a4f5e18bf3ab8264cc9daf8
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-21 02:39:26 -0500

    clock Img

--- src/assets/img-index.js
+++ src/assets/img-index.js
@@ -1,10 +1,11 @@
 import MainImg from './img/image-equilibrium.jpg'
 import AvatarImg from './img/image-avatar.png'
 import EthImg from './svg/icon-ethereum.svg'
+import ClockImg from './svg/icon-clock.svg'
 
 export const images = {
   img: { MainImg, AvatarImg },
-  svg: { EthImg },
+  svg: { EthImg, ClockImg },
   tiny: {},
 }
 
