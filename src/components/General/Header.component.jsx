465ed1388b69825a4999858c7d038bcb65e618b2
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-21 01:21:20 -0500

    main layout set

--- src/components/General/Header.component.jsx
+++ src/components/General/Header.component.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
-const HeaderStyles = 'pt-60 pb-6 w-full flex justify-center'
+const HeaderStyles = 'pt-36 pb-6 w-full flex justify-center'
 
 const Header = () => {
-  return (
-    <header className={HeaderStyles}>
-      <h1>Let's get started building components...</h1>
-    </header>
-  )
+  return <header className={HeaderStyles}></header>
 }
 
 export default Header
