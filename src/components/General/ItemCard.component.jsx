26ec44e3e73ddccfd277a00e3989ce4a6dfcb2cd
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-21 01:21:44 -0500

    main card base

---
+++ src/components/General/ItemCard.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const arbStyles = {
+  wrapper: {
+    height: '540px',
+    width: '325px',
+  },
+}
+
+const ItemCard = () => {
+  return (
+    <div className="bg-darkBlue-bgCard" style={arbStyles.wrapper}>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default ItemCard
--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,7 +1,8 @@
-import Header from './General/Header.component.jsx'
-import Layout from './General/Layout.component.jsx'
+import Header from "./General/Header.component.jsx"
+import ItemCard from "./General/ItemCard.component.jsx"
+import Layout from "./General/Layout.component.jsx"
 export const CIndex = {
-  Header,
-  Layout,
-}
-export default CIndex
+		Header,ItemCard,Layout
+	}
+	export default CIndex
+	
